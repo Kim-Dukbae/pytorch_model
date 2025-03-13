a6018c2386ff2ddd0d0317b44999ef8bd8f0a0f7
김덕배 <97142351+Kimduckba@users.noreply.github.com>
2025-03-13 10:34:04 +0900

    Update layers.py

--- layers/layers.py
+++ layers/layers.py
@@ -1,34 +1,36 @@
+import torch
+import torch.nn as nn
 
-class Convolution(nn.Module):
-    """
-    Convolution Layer Block
-
-    Args:
-        in_channels (int): Number of input channels.
-        count (int): Number of (convolution => [BN] => ReLU) blocks to apply.
-        Share (bool): Whether to share weights between the blocks.
-
-    Structure:
-        (convolution => [BN] => ReLU) * count
-    """
-    def __init__(self, in_channels, output_channels, count=2, Share= False):
-        super().__init__()
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels,
+                 kernel_size= 3, stride= 1, padding= 1, bias= False,
+                 count= 2):
+        super(ConvBlock, self).__init__()  
         layers = []
-        layers.append(nn.Conv2d(in_channels, output_channels, kernel_size=3, padding=1, bias=False))
-        layers.append(nn.BatchNorm2d(output_channels))
-        layers.append(nn.ReLU(inplace=True))
         for _ in range(count):
-            layers.append(nn.Conv2d(output_channels, output_channels, kernel_size=3, padding=1, bias=False))
-            layers.append(nn.BatchNorm2d(output_channels))
+            layers.append(nn.Conv2d(
+                in_channels=in_channels if _ == 0 else out_channels,
+                out_channels=out_channels,
+                kernel_size= kernel_size,
+                stride= stride,
+                padding= padding,
+                bias = bias
+            ))
+            if bias == False:
+                layers.append(nn.BatchNorm2d(out_channels))
             layers.append(nn.ReLU(inplace=True))
 
-        self.conv = nn.Sequential(*layers)
-
-        if Share:
-            for i in range(3, len(self.conv), 3):  # 3의 배수 인덱스를 반복
-                self.conv[i].weight = nn.Parameter(self.conv[0].weight.clone())
-                self.conv[i].weight.requires_grad = False
-
+        self.layers = nn.Sequential(*layers)
 
     def forward(self, x):
-        return self.conv(x)
+        return self.layers(x)
+
+class Fully_Connected(nn.Module):
+    def __init__(self, in_channels, hidden_channels, num_classes):
+        super(Fully_Connected, self).__init__()
+        self.fc1 = nn.Linear(in_features= in_channels, out_features= hidden_channels)
+        self.fc2 = nn.Linear(in_features= hidden_channels, out_features= num_classes)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.fc2( self.relu( self.fc1(x) ))
