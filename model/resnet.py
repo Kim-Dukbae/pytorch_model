df582bb6d6cbc7d3eaad273542bd7aea2cc94f17
김덕배 <97142351+Kim-Dukbae@users.noreply.github.com>
2025-03-15 21:18:52 +0900

    Update resnet.py

--- model/resnet.py
+++ model/resnet.py
@@ -8,33 +8,33 @@ ResNet151
 
 class ResNet18(nn.Module):
     def __init__(self, in_channels= 3, out_channels= 64, img_size= 224, hidden_channels= 512, num_classes=1000):
-        super(ResNet18, self).__init__()  
+        super(ResNet18, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, 
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
                       kernel_size=7, stride=2, padding=3, bias= False),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),  # 활성화 함수로 ReLU 추가
+            nn.ReLU(inplace=True), 
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1)
         )
         self.layer2 = nn.Sequential(
-            Residual_Block(out_channels, stride=1, downsample= False),
-            Residual_Block(out_channels, stride=1, downsample= False)
+            ResidualBlock(out_channels, downsample= False),
+            ResidualBlock(out_channels, downsample= False)
         )
         self.layer3 = nn.Sequential(
-            Residual_Block(out_channels, stride=2, downsample= True),
-            Residual_Block(out_channels*2, stride=1, downsample= False)
+            ResidualBlock(out_channels, downsample= True),
+            ResidualBlock(out_channels*2, downsample= False)
         )
         self.layer4 = nn.Sequential(
-            Residual_Block(out_channels*2, stride=2, downsample= True),
-            Residual_Block(out_channels*4, stride=1, downsample= False)
+            ResidualBlock(out_channels*2, downsample= True),
+            ResidualBlock(out_channels*4, downsample= False)
         )
         self.layer5 = nn.Sequential(
-            Residual_Block(out_channels*4, stride=2, downsample= True),
-            Residual_Block(out_channels*8, stride=1, downsample= False)
+            ResidualBlock(out_channels*4, downsample= True),
+            ResidualBlock(out_channels*8, downsample= False)
         )
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(in_features= out_channels*8, out_features= num_classes)
-        
+
     def forward(self, x):
         x = self.layer1(x)
         x = self.layer2(x)
@@ -52,24 +52,24 @@ class ResNet34(nn.Module):
             nn.Conv2d(in_channels=in_channels, out_channels=out_channels, 
                       kernel_size=7, stride=2, padding=3, bias= False),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),  # 활성화 함수로 ReLU 추가
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1)
         )
         self.layer2 = nn.Sequential(
-            Residual_Block(out_channels, stride=1, downsample= False),
-            *[Residual_Block(out_channels, stride=1, downsample=False) for _ in range(2)]
+            ResidualBlock(out_channels, downsample= False),
+            *[ResidualBlock(out_channels, downsample= False) for _ in range(2)]
         )
         self.layer3 = nn.Sequential(
-            Residual_Block(out_channels, stride=2, downsample= True),
-            *[Residual_Block(out_channels*2, stride=1, downsample=False) for _ in range(3)]
+            ResidualBlock(out_channels, downsample= True),
+            *[Residual_Block(out_channels*2, downsample=False) for _ in range(3)]
         )
         self.layer4 = nn.Sequential(
-            Residual_Block(out_channels*2, stride=2, downsample=True),
-            *[Residual_Block(out_channels*4, stride=1, downsample=False) for _ in range(5)]
+             ResidualBlock(out_channels*2, downsample= True),
+            *[Residual_Block(out_channels*4, downsample=False) for _ in range(5)]
         )
         self.layer5 = nn.Sequential(
-            Residual_Block(out_channels*4, stride=2, downsample= True),
-            *[Residual_Block(out_channels*8, stride=1, downsample=False) for _ in range(2)]
+             ResidualBlock(out_channels*4, downsample= True),
+            *[Residual_Block(out_channels*8, downsample=False) for _ in range(2)]
         )
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(in_features= out_channels*8, out_features= num_classes)
