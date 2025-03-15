fa944cd05dfaa6814d81183fbcecf9aa59e91723
김덕배 <97142351+Kim-Dukbae@users.noreply.github.com>
2025-03-15 21:14:55 +0900

    Update residual.py

--- layers/residual.py
+++ layers/residual.py
@@ -1,27 +1,38 @@
-class Residual_Block(nn.Module):
-    def __init__(self, in_channels, stride=1, downsample=False):
-        super(Residual_Block, self).__init__()
-        out_channels = in_channels if stride == 1 else in_channels * 2
-        self.residual = downsample or stride != 1  # stride=2면 무조건 다운샘플링
+import torch.nn as nn
 
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        
-        if self.residual:
-            self.downsample = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels)
-            )
-        
-        self.relu = nn.ReLU(inplace=True)
-    
+# Resnet18/34에서 활용되는 layers
+class Downsample(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Downsample, self).__init__()
+        self.downsample = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=2,padding=0, bias=False),
+            nn.BatchNorm2d(out_channels)
+        )
+
     def forward(self, x):
-        residual = self.bn2(self.conv2(self.relu(self.bn1(self.conv1(x)))))
+        return self.downsample(x)
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, downsample= False):
+        super(ResidualBlock, self).__init__()
+        self.downsample = None
+        stride = 2 if downsample ==  True  else 1
+        out_channels = in_channels if stride == 1 else in_channels * 2
+
+        self.conv = ConvBlock( in_channels, out_channels ,
+                 kernel_size= 3, stride= stride, padding= 1, bias= False,
+                 count= 1)
         
-        if self.residual:
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 
+                               kernel_size= 3, stride= 1, padding= 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+        if downsample:
+            self.downsample = Downsample(in_channels, out_channels)
+            
+    def forward(self, x):
+        residual = self.bn2(self.conv2(self.conv(x) ))
+        if self.downsample is not None:
             x = self.downsample(x)
-        
-        return self.relu(residual + x)
+        return self.relu(x + residual)
